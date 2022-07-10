007b7a735cd8773b66a42d2416b3c7cb5702e835
Vincent <vpark45@gmail.com>
2022-07-10 22:54:13 +0000

    Test Trivial

---
+++ app/Main.hs
@@ -0,0 +1,11 @@
+module Main where
+
+import Test.QuickCheck
+import Trivial
+
+semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+main :: IO ()
+main = do
+  quickCheck (semigroupAssoc :: TrivAssoc)
